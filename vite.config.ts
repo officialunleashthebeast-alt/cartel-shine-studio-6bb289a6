0de3d3ed716c55a9f0f8ab51459ffd9fd08879f0
officialunleashthebeast-alt <officialunleashthebeast@example.com>
2026-03-13 00:59:59 +0530

    ban gya

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/cartel-shine-studio/",
+  base: "/cartel-shine-studio-6bb289a6/",
   server: {
     host: "::",
     port: 8080,
